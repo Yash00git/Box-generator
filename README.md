d00542b891849242247c5a03fd28938f34d7cdfa
Yash khandelwal <65126639+Yash00git@users.noreply.github.com>
2022-08-04 13:07:19 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Box generator
- 
+#  Moveable box generator
+#### How to Start the Application:
+```
+1=>clone the repo
+2=>cd into the repo
+3=>type npm install in the terminal
+4=>type npm start
+5=>Deployed version link https://flytbase-box.herokuapp.com/
+```
+#### Important Points:
+```
+1=>The brown dash lines represent the fence
+2=>The add button will add boxes which have a unique number on them
+3=>only when the toggle is On ,then only the boxes can be moved
+4=>Use W,A,S,D to move the boxes inside the fence
+5=>Press the delete button on the keyboard to delete the box
+6=>No npm libraries are used in the process except node sass to use scss instead of normal css.
+```
+
---
+++ src/Pages/Main.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import './Main.css';
+import Box from '../Components/Box';
+
+class Main extends Component {
+    state = {
+        count:1,
+        box:[
+            {
+            id:1
+            }
+        ],
+        isToggle:false
+    }
+    handleToggle=()=>{
+        this.setState({isToggle:!this.state.isToggle})
+    }
+    handleBox=()=>{
+        if(this.state.isToggle)
+        {
+        this.setState({box:[...this.state.box,{id:this.state.count+1}]})
+        this.setState({count:this.state.count+1})
+        }
+    }
+    handleDel=(idd)=>
+    {
+        this.setState(prevState => {
+            const box = prevState.box.filter(task => task.id !== idd);
+            return { box };
+        });
+    }
+    render() { 
+        console.log(this.state)
+        return (<div className="main">
+            <div className="t">
+            <div className="left"><button onClick={this.handleBox}>Add Box</button></div>
+            <div className="right">
+                <button onClick={this.handleToggle} value={this.state.isToggle} className={this.state.isToggle?'on':'off'}>{this.state.isToggle?'ON':"OFF"}</button>
+                </div>
+            </div>
+            <div className='arena'>
+                {this.state.box.map(val=><Box key={val.id} v={val.id} action={this.handleDel} t={this.state.isToggle}></Box>)}
+           
+            </div>
+           
+        </div> );
+    }
+}
+ 
+export default Main;
